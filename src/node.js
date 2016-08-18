26c8b0d6a087badc8ce1b514ed32653685200d87
Gleb <shevcov.gleb@gmail.com>
2016-08-18 09:25:38 +0300

    Update removeChild Node class

--- src/node.js
+++ src/node.js
@@ -18,7 +18,17 @@ class Node {
 	}
 
 	removeChild(node) {
-		
+		try {
+			if(node.data == this.left.data && node.priority == this.left.priority) {
+				this.left.parent = null;
+				this.left = null;
+			} else if(node.data == this.right.data && node.priority == this.right.priority) {
+				this.right.parent = null;
+				this.right = null;
+			} else {
+				throw new SyntaxError("Passed node is not a child of this node");
+			}
+		}
 	}
 
 	remove() {
