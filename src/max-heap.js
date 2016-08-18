0def72c9a5a04c9759f0a50a891eef95d57a3b38
Gleb <shevcov.gleb@gmail.com>
2016-08-18 17:19:10 +0300

    Update max-heap

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,7 +2,8 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
@@ -34,6 +35,22 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
+		if(this.parentNodes.length == 0) {
+			this.root = node;
+			this.parentNodes.push(node);
+		}
+		var heap = this.parentNodes;
+		for(var i = 0; i < heap.length; i++) {
+			if(heap[i].left == null) {
+				heap[i].left = node;
+				node.parent = heap[i];
+				heap.push(node);
+			} else if (heap[i].right == null) {
+				heap[i].right = node;
+				node.parent = heap[i];
+				heap.push(node);
+			}
+		}
 		
 	}
 
