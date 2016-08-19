81518e025cacf7bb6ceee00372d98dd5002eb450
Gleb <shevcov.gleb@gmail.com>
2016-08-19 16:33:00 +0300

    conctructor queue

--- src/queue.js
+++ src/queue.js
@@ -2,7 +2,10 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if(maxSize) this.maxSize = maxSize;
+		else this.maxSize = 30;
+		
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
