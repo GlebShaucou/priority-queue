878b2bc6e27db74169ebe731956398dd4eab8490
GlebShaucou <shevcov.gleb@gmail.com>
2016-08-21 13:49:17 +0300

    global update

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const Node = require('./src/node');
 const h = new MaxHeap();
 window.h = h;
 
-h.push(0, 0);
-			h.push(15, 2);
-			h.push(42, 13);
+h.root = new Node(0, 10);
+			h.root.appendChild(new Node(1, 5));
+			h.root.appendChild(new Node(2, 7));
+			h.root.left.appendChild(new Node(3, 20));
 
-      console.log(h);
+/**
+        10                       20
+       /  \                     /  \
+      5    7  - shift up ->   10   7
+     /                        /
+    20                       5
+**/
+
+			h.parentNodes = [
+				h.root.left,
+				h.root.right,
+				h.root.left.left,
+			];
+
+			h.shiftNodeUp(h.root.left.left);
+			console.log(h);
--- src/max-heap.js
+++ src/max-heap.js
@@ -9,173 +9,201 @@ class MaxHeap {
 	}
 
 	push(data, priority) {
-		this.insertNode(new Node(data, priority));
-		this.shiftNodeUp(this.parentNodes[this.parentNodes.length-1]);
+		// this.insertNode(new Node(data, priority));
+		// this.shiftNodeUp(this.parentNodes[this.parentNodes.length-1]);
 	}
 
 	pop() {
-		if(!this.isEmpty()) {
-			var retData = this.root.data;
-			var tempArr = [];
-			var newRoot = this.detachRoot();
-			tempArr.push(newRoot);
-			for(var i = 1; i < this.parentNodes.length; i++) {
-				tempArr[i] = this.parentNodes[i];
-			}
-			this.clear();
-			for (var i = 0; i < tempArr.length; i++) {
-				this.push(tempArr[i].data, tempArr[i].priority);
-			}
-
-			return retData;
-		}
+		// if(!this.isEmpty()) {
+		// 	var retData = this.root.data;
+		// 	var tempArr = [];
+		// 	var newRoot = this.detachRoot();
+		// 	tempArr.push(newRoot);
+		// 	for(var i = 1; i < this.parentNodes.length; i++) {
+		// 		tempArr[i] = this.parentNodes[i];
+		// 	}
+		// 	this.clear();
+		// 	for (var i = 0; i < tempArr.length; i++) {
+		// 		this.push(tempArr[i].data, tempArr[i].priority);
+		// 	}
+		//
+		// 	return retData;
+		// }
 	}
 
 	detachRoot() {
-		var root = this.root;
-		this.root = null;
-		//this.parentNodes.unshift(root);
-		return this.parentNodes[0];
+		// var root = this.root;
+		// this.root = null;
+		// //this.parentNodes.unshift(root);
+		// return this.parentNodes[0];
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		var lastInsertedNode = this.parentNodes.pop();
-		lastInsertedNode.parent.right = null;
-		this.root = lastInsertedNode;
-		detached.left.parent = lastInsertedNode;
-		detached.right.parent = lastInsertedNode;
-		lastInsertedNode.left = detached.left;
-		lastInsertedNode.right = detached.right;
+		// var lastInsertedNode = this.parentNodes.pop();
+		// lastInsertedNode.parent.right = null;
+		// this.root = lastInsertedNode;
+		// detached.left.parent = lastInsertedNode;
+		// detached.right.parent = lastInsertedNode;
+		// lastInsertedNode.left = detached.left;
+		// lastInsertedNode.right = detached.right;
 	}
 
 	size() {
-		if(!this.isEmpty()) {
-			return this.parentNodes.length + 1;
-		}
-		return 0;
+		// if(!this.isEmpty()) {
+		// 	return this.parentNodes.length + 1;
+		// }
+		// return 0;
 	}
 
 	isEmpty() {
-		return this.heapEmpty;
+		//return this.heapEmpty;
 	}
 
 	clear() {
-		this.root = null;
-		this.parentNodes.length  = 0;
-		//this.heapTree.length = 0;
-		this.heapEmpty = true;
+		// this.root = null;
+		// this.parentNodes.length  = 0;
+		// //this.heapTree.length = 0;
+		// this.heapEmpty = true;
 	}
 
 	insertNode(node) {
-		if(this.parentNodes.length == 0 && this.root == null) {
+		if(this.heapTree.length == 0) {
 			this.root = node;
-			//node.index = 0;
-			this.parentNodes.push(node);
 			this.heapTree.push(node);
-			this.heapEmpty = false;
 		} else {
 			var i = 0;
-			while(node.parent == null){
-				if(this.parentNodes[i].left == null) {
-					this.parentNodes.push(node);
+			while (node.parent == null) { //добавляем ноды в дерево
+				if(this.heapTree[i].left == null) {
+					this.heapTree[i].left = node;
+					node.parent = this.heapTree[i];
 					this.heapTree.push(node);
-					//node.index = this.heapTree.length-1;
-					node.parent = this.parentNodes[i];
-					this.parentNodes[i].left = node;
-					i++;
 					continue;
-				} else if(this.parentNodes[i].right == null) {
-					this.parentNodes.push(node);
+				}
+				if(this.heapTree[i].right == null) {
+					this.heapTree[i].right = node;
+					node.parent = this.heapTree[i];
 					this.heapTree.push(node);
-					//node.index = this.heapTree.length-1;
-					node.parent = this.parentNodes[i];
-					this.parentNodes[i].right = node;
-					i++;
 					continue;
 				}
 				i++;
 			}
-			if(this.parentNodes[0].left != null && this.parentNodes[0].right != null) {
-				this.parentNodes.shift();
-			}
 		}
+		this.maintainParentNodes(); //готовим parentNodes
+	}
+
+	maintainParentNodes() { //готовим parentNodes
+		this.parentNodes.length = 0;
+			for (var i = 0; i < this.heapTree.length; i++) {
+				if(this.heapTree[i].left == null || this.heapTree[i].right == null) {
+					this.parentNodes.push(this.heapTree[i]);
+				}
+			}
 	}
 
 	shiftNodeUp(node) {
-		var $this = this;
+		//var $this = this;
 		if(node.parent == null) {
-			$this.root = node;
-			//orderParentNodes($this);
+			this.root = node;
+		} else if(node.priority > node.parent.priority) {
+			if(node.priority > node.parent.left.priority) {
+				swapWithLeft(node);
+			}
+			node.swapWithParent();
+			this.shiftNodeUp(node);
+		} else {
 			return;
 		}
-		//делаем parentNodes на основе упорядоченной кучи
-		// function orderParentNodes($this, nodeInd, parentInd) {
-		// 	if(node.parent != null) {
-		// 		var temp = $this.heapTree[nodeInd];
-		// 		$this.heapTree[nodeInd] = $this.heapTree[parentInd];
-		// 		$this.heapTree[parentInd] = temp;
-		// 		$this.parentNodes.length = 0;
-		// 		for(var i = 0; i < $this.heapTree.length; i++) {
-		// 			$this.parentNodes.push($this.heapTree[i]);
-		// 		}
-		// 	}
-		// }
-
-
-		//перемещаем ноды вверх, выполняется условие кучи
-		if(node.priority > node.parent.priority) {
-
-			if(node.priority > node.parent.left.priority) {// боковая перестановка ноды с большим приоритетом находятся левее
-					var temp;
-					temp = node.parent.right;
-					node.parent.right = node.parent.left;
-					node.parent.left = temp;
-
-					temp = node.left;
-					node.left = node.parent.right.left;
-					node.parent.right.left = temp;
-
-					temp = node.right;
-					node.right = node.parent.right.right;
-					node.parent.right.right = temp;
-					if(node.left != null) {
-							node.left.parent = node;
-					}
-					if(node.right != null) {
-							node.right.parent = node;
-					}
-					if(node.parent.right.left != null) {
-							node.parent.right.left = node.parent.right;
-					}
-					if(node.parent.right.right != null) {
-							node.parent.right.right = node.parent.right;
-					}
-					//приводим в порядок parentNodes после боковой перестановки
-					// temp = $this.parentNodes[0];
-					// $this.parentNodes[0] = $this.parentNodes[1];
-					// $this.parentNodes[1] = temp;
-					//orderParentNodes($this, node.index, node.parent.right.index);
-			 	}
-
-				if(node.parent.data == $this.root.data && node.parent.priority == $this.root.priority) {
-					// var temp;
-					// temp = $this.root;
-					$this.root = node;
-					//$this.parentNodes.unshift(temp);
-					//
-					// temp = $this.parentNodes[0];
-					// $this.parentNodes[0] = $this.parentNodes[1];
-					// $this.parentNodes[1] = temp;
-					node.swapWithParent();
-					return;
+		this.heapTree.length = 0;
+		this.heapTree.push(this.root);
+		var i = 0;
+		while (this.heapTree[i].left != null) {
+				this.heapTree.push(this.heapTree[i].left);
+				if(this.heapTree[i].right != null) {
+					this.heapTree.push(this.heapTree[i].right)
 				}
-
-				//orderParentNodes($this, node.index, node.parent.index);
-				node.swapWithParent();
-				this.shiftNodeUp(node);
-		}
-		//orderParentNodes($this);
+				i++;
+			}
+		this.maintainParentNodes();
 		return;
+
+		function swapWithLeft(node) {
+			var temp;
+			temp = node.parent.right;
+			node.parent.right = node.parent.left;
+			node.parent.left = temp;
+
+			temp = node.left;
+			node.left = node.parent.right.left;
+			node.parent.right.left = temp;
+
+			temp = node.right;
+			node.right = node.parent.right.right;
+			node.parent.right.right = temp;
+			if(node.left != null) {
+					node.left.parent = node;
+			}
+			if(node.right != null) {
+					node.right.parent = node;
+			}
+			if(node.parent.right.left != null) {
+					node.parent.right.left = node.parent.right;
+			}
+			if(node.parent.right.right != null) {
+					node.parent.right.right = node.parent.right;
+			}
+		}
+
+		// var $this = this;
+		// if(node.parent == null) {
+		// 	$this.root = node;
+		// 	//orderParentNodes($this);
+		// 	return;
+		// }
+		// //делаем parentNodes на основе упорядоченной кучи
+		// // function orderParentNodes($this, nodeInd, parentInd) {
+		// // 	if(node.parent != null) {
+		// // 		var temp = $this.heapTree[nodeInd];
+		// // 		$this.heapTree[nodeInd] = $this.heapTree[parentInd];
+		// // 		$this.heapTree[parentInd] = temp;
+		// // 		$this.parentNodes.length = 0;
+		// // 		for(var i = 0; i < $this.heapTree.length; i++) {
+		// // 			$this.parentNodes.push($this.heapTree[i]);
+		// // 		}
+		// // 	}
+		// // }
+		//
+		//
+		// //перемещаем ноды вверх, выполняется условие кучи
+		// if(node.priority > node.parent.priority) {
+		//
+		// 	if(node.priority > node.parent.left.priority) {// боковая перестановка ноды с большим приоритетом находятся левее
+
+		// 			//приводим в порядок parentNodes после боковой перестановки
+		// 			// temp = $this.parentNodes[0];
+		// 			// $this.parentNodes[0] = $this.parentNodes[1];
+		// 			// $this.parentNodes[1] = temp;
+		// 			//orderParentNodes($this, node.index, node.parent.right.index);
+		// 	 	}
+		//
+		// 		if(node.parent.data == $this.root.data && node.parent.priority == $this.root.priority) {
+		// 			// var temp;
+		// 			// temp = $this.root;
+		// 			$this.root = node;
+		// 			//$this.parentNodes.unshift(temp);
+		// 			//
+		// 			// temp = $this.parentNodes[0];
+		// 			// $this.parentNodes[0] = $this.parentNodes[1];
+		// 			// $this.parentNodes[1] = temp;
+		// 			node.swapWithParent();
+		// 			return;
+		// 		}
+		//
+		// 		//orderParentNodes($this, node.index, node.parent.index);
+		// 		node.swapWithParent();
+		// 		this.shiftNodeUp(node);
+		// }
+		// //orderParentNodes($this);
+		// return;
 
 	}
 
